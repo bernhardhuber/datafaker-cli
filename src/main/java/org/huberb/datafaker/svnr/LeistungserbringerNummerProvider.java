12c6669597f6633818eeca3b31640b1a4621adca
Bernhard Huber <bernihuber123@gmail.com>
2023-04-25 22:25:20 +0200

    add draft vpnr, lenr

---
+++ src/main/java/org/huberb/datafaker/svnr/LeistungserbringerNummerProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.datafaker.svnr;
+
+import net.datafaker.providers.base.AbstractProvider;
+import net.datafaker.providers.base.BaseProviders;
+
+/**
+ *
+ * @author berni3
+ * @since 1.0
+ */
+public class LeistungserbringerNummerProvider extends AbstractProvider<BaseProviders> {
+
+    public LeistungserbringerNummerProvider(BaseProviders faker) {
+        super(faker);
+    }
+
+    public String vpnr() {
+        // TODO check well-define length, and alphabet
+        String result = faker.numerify("######");
+        return result;
+    }
+
+    public String lenr() {
+        // TODO check well-define length, and alphabet
+        String result = faker.numerify("########");
+        return result;
+    }
+}
---
+++ src/test/java/org/huberb/datafaker/svnr/LeistungserbringerNummerProviderTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.datafaker.svnr;
+
+import java.util.Locale;
+import java.util.function.Predicate;
+import net.datafaker.Faker;
+import org.huberb.datafaker.cli.Adapters;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author berni3
+ */
+public class LeistungserbringerNummerProviderTest {
+
+    static Faker faker;
+    static Predicate<String> allDigitsPredicate = s -> {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    @BeforeAll
+    public static void setUpAll() {
+        faker = Adapters.FakerFactory.createFakerFromLocale(Locale.getDefault());
+    }
+    LeistungserbringerNummerProvider instance;
+
+    @BeforeEach
+    public void setUp() {
+        instance = new LeistungserbringerNummerProvider(faker);
+    }
+
+    /**
+     * Test of vpnr method, of class LeistungserbringerNummerProvider.
+     */
+    @Test
+    public void testVpnr() {
+        String result = instance.vpnr();
+        assertAll(
+                () -> assertEquals(6, result.length()),
+                () -> assertTrue(allDigitsPredicate.test(result), "" + result)
+        );
+    }
+
+    /**
+     * Test of lenr method, of class LeistungserbringerNummerProvider.
+     */
+    @Test
+    public void testLenr() {
+        String result = instance.lenr();
+        assertAll(
+                () -> assertEquals(8, result.length()),
+                () -> assertTrue(allDigitsPredicate.test(result), "" + result)
+        );
+    }
+}
