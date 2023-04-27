ab3dc5c20ca122a36b989fac719759af68607eab
Bernhard Huber <bernihuber123@gmail.com>
2023-04-27 22:02:27 +0200

    reorg packages name

--- src/main/java/org/huberb/datafaker/samples/Icd3DiseasesYaml.java
+++ src/main/java/org/huberb/datafaker/samples/Icd3DiseasesYaml.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.huberb.datafaker.svnr;
+package org.huberb.datafaker.samples;
 
 import java.nio.file.Paths;
 import java.util.Locale;
--- src/main/java/org/huberb/datafaker/samples/LeistungserbringerNummerProvider.java
+++ src/main/java/org/huberb/datafaker/samples/LeistungserbringerNummerProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.huberb.datafaker.svnr;
+package org.huberb.datafaker.samples;
 
 import net.datafaker.providers.base.AbstractProvider;
 import net.datafaker.providers.base.BaseProviders;
