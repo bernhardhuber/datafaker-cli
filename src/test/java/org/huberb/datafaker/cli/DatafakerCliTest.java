b7fef2a0cf2271517a3dd08d7a7b5c62cca3119a
Bernhard Huber <bernihuber123@gmail.com>
2023-05-03 21:28:24 +0200

    add more commandline test

--- src/test/java/org/huberb/datafaker/cli/DatafakerCliTest.java
+++ src/test/java/org/huberb/datafaker/cli/DatafakerCliTest.java
@@ -18,13 +18,21 @@ package org.huberb.datafaker.cli;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.huberb.datafaker.cli.DatafakerCli.AvailableModes;
+import org.huberb.datafaker.cli.DatafakerCli.DataModes;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import picocli.CommandLine;
 
 /**
@@ -45,17 +53,13 @@ public class DatafakerCliTest {
      */
     @Test
     public void testMain_no_args() throws IOException {
-        CommandLine cmd = new CommandLine(instance);
-        try (StringWriter swErr = new StringWriter(); StringWriter swOut = new StringWriter()) {
-            try (PrintWriter err = new PrintWriter(swErr); PrintWriter out = new PrintWriter(swOut)) {
-                cmd.setErr(err);
-                cmd.setOut(out);
-                int exitCode = cmd.execute("");
-                assertEquals(0, exitCode);
-            }
-            swErr.flush();
-            swOut.flush();
-            /* Sample:
+        String[] resultsOutErr = new Invocation()
+                .instance(new DatafakerCli())
+                .args()
+                .invoke();
+        String swOut = resultsOutErr[0];
+        String swErr = resultsOutErr[1];
+        /* Sample:
                 Hello org.huberb.datafaker.cli.DatafakerCli
                 expression: locale: en_US
                 fieldname name
@@ -66,23 +70,160 @@ public class DatafakerCliTest {
                 "Tobie Goldner","Suite 889 74374 Dean Trail, Lake Wilfredo, LA 43007"
                 "Mrs. Lou Kohler","Apt. 059 0867 Cora Lock, North Jacqualinetown, NC 30080"
                 "Beaulah Reichert","449 Becker Plains, North Harrisonside, NY 95819"
-             */
-            assertEquals("", swErr.toString());
-            String outResult = swOut.toString();
-            String m = "" + outResult;
+         */
+        assertEquals("", swErr);
+        String outResult = swOut;
+        String m = "" + outResult;
 
-            String pattern = "\"[A-Za-z0-9 ,.\"]*\",\"[A-Za-z0-9 ,.]*\"";
-            Matcher matcher = Pattern.compile(pattern).matcher(outResult);
-            assertTrue(matcher.find(), m);
+        String pattern = "\"[A-Za-z0-9 ,.\"]*\",\"[A-Za-z0-9 ,.]*\"";
+        Matcher matcher = Pattern.compile(pattern).matcher(outResult);
+        assertTrue(matcher.find(), m);
 
-            assertAll(
-                    () -> assertTrue(outResult.contains("Hello org.huberb.datafaker.cli.DatafakerCli"), m),
-                    () -> assertTrue(outResult.contains("locale:"), m),
-                    () -> assertTrue(outResult.contains("fieldname name"), m),
-                    () -> assertTrue(outResult.contains("fieldname address"), m),
-                    () -> assertTrue(outResult.contains("\"name\",\"address\""), m)
-            );
-        }
+        assertAll(
+                () -> assertTrue(outResult.contains("Hello org.huberb.datafaker.cli.DatafakerCli"), m),
+                () -> assertTrue(outResult.contains("locale:"), m),
+                () -> assertTrue(outResult.contains("fieldname name"), m),
+                () -> assertTrue(outResult.contains("fieldname address"), m),
+                () -> assertTrue(outResult.contains("\"name\",\"address\""), m)
+        );
     }
 
+    /**
+     * Test of main method, of class DatafakerCli.
+     */
+    @ParameterizedTest
+    @EnumSource(value = AvailableModes.class)
+    public void testMain_available(AvailableModes availableModes) throws IOException {
+        String m = "" + availableModes;
+        String[] resultsOutErr = new Invocation()
+                .instance(new DatafakerCli())
+                .args("--available=" + availableModes.name())
+                .invoke();
+        String swOut = resultsOutErr[0];
+        String swErr = resultsOutErr[1];
+
+        assertAll(
+                () -> assertNotNull(swErr, m),
+                () -> assertEquals("", swErr, m)
+        );
+        assertAll(
+                () -> assertNotNull(swOut, m),
+                () -> assertFalse(swOut.isEmpty(), m)
+        );
+        assertAll(
+                () -> assertFalse(swOut.contains("Usage:"), m),
+                () -> assertFalse(swOut.contains("--help"), m),
+                () -> assertFalse(swOut.contains("--version"), m),
+                () -> assertFalse(swOut.contains("Run datafaker from the command line"), m)
+        );
+    }
+
+    /**
+     * Test of main method, of class DatafakerCli.
+     */
+    @ParameterizedTest
+    @EnumSource(value = DataModes.class)
+    public void testMain_expression(DataModes dataModes) throws IOException {
+        String m = "" + dataModes;
+        String[] resultsOutErr = new Invocation()
+                .instance(new DatafakerCli())
+                .args("--expression=" + dataModes.name(),
+                        "--count=" + 3,
+                        "--locale=en-US"
+                )
+                .invoke();
+        String swOut = resultsOutErr[0];
+        String swErr = resultsOutErr[1];
+
+        assertAll(
+                () -> assertNotNull(swErr, m),
+                () -> assertEquals("", swErr, m)
+        );
+        assertAll(
+                () -> assertNotNull(swOut, m),
+                () -> assertFalse(swOut.isEmpty(), m)
+        );
+        assertAll(
+                () -> assertFalse(swOut.contains("Usage:"), m),
+                () -> assertFalse(swOut.contains("--help"), m),
+                () -> assertFalse(swOut.contains("--version"), m),
+                () -> assertFalse(swOut.contains("Run datafaker from the command line"), m)
+        );
+    }
+
+    /**
+     * Test of main method, of class DatafakerCli.
+     */
+    @Test
+    public void testMain_expression_parameters() throws IOException {
+        List<String> parameters = Arrays.asList("#{Name.fullName}",
+                "#{Address.fullAddress}"
+        );
+        String m = "" + parameters;
+        String[] resultsOutErr = new Invocation()
+                .instance(new DatafakerCli())
+                .args("--expression=expression",
+                        "--count=" + 3,
+                        "--locale=en-US",
+                        parameters.get(0),
+                        parameters.get(1)
+                )
+                .invoke();
+        String swOut = resultsOutErr[0];
+        String swErr = resultsOutErr[1];
+
+        assertAll(
+                () -> assertNotNull(swErr, m),
+                () -> assertEquals("", swErr, m)
+        );
+        assertAll(
+                () -> assertNotNull(swOut, m),
+                () -> assertFalse(swOut.isEmpty(), m)
+        );
+        assertAll(
+                () -> assertFalse(swOut.contains("Usage:"), m),
+                () -> assertFalse(swOut.contains("--help"), m),
+                () -> assertFalse(swOut.contains("--version"), m),
+                () -> assertFalse(swOut.contains("Run datafaker from the command line"), m)
+        );
+    }
+
+    /**
+     * Convenience wrapper invoking {@link DatafakerCli} with command line
+     * arguments.
+     */
+    static class Invocation {
+
+        DatafakerCli instance;
+        String[] args;
+
+        Invocation instance(DatafakerCli instance) {
+            this.instance = instance;
+            return this;
+        }
+
+        Invocation args(String... args) {
+            this.args = args;
+            return this;
+        }
+
+        String[] invoke() throws IOException {
+            CommandLine cmd = new CommandLine(instance);
+            try (StringWriter swErr = new StringWriter(); StringWriter swOut = new StringWriter()) {
+                try (PrintWriter err = new PrintWriter(swErr); PrintWriter out = new PrintWriter(swOut)) {
+                    cmd.setErr(err);
+                    cmd.setOut(out);
+                    int exitCode = cmd.execute(args);
+                    assertEquals(0, exitCode);
+                }
+                swErr.flush();
+                swOut.flush();
+                String[] result = new String[2];
+                result[0] = swOut.toString();
+                result[1] = swErr.toString();
+                return result;
+            }
+        }
+
+    }
 }
